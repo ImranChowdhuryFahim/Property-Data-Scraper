5e3651be09a39b86bd41815037c0cead83a5d17d
Imran Chowdhury <imran.cuet.cse17@gmail.com>
2023-07-12 02:30:37 +0600

    integrated github actions to deploy lambda function

---
+++ scraper-service/src/app.ts
@@ -0,0 +1,10 @@
+import express, { Request, Response } from 'express';
+import serverless from 'serverless-http';
+
+const app = express();
+
+app.use('/', (req: Request, res: Response) => { res.status(200).json({ message: 'hello from aws-nodejs-lambda' }) });
+
+
+export default app;
+export const handler = serverless(app);

19c9d5879bfa899c00d637dff35a8ea1afc34795
Imran Chowdhury <imran.cuet.cse17@gmail.com>
2023-07-13 10:11:49 +0600

    2 promise

--- scraper-service/src/services/scraper.service.ts
+++ scraper-service/src/services/scraper.service.ts
@@ -5,7 +5,7 @@ class ScraperService {
     async scrape() {
         const list: DataTobeScraped[] = [
             { name: 'Brookdale Creekside', state: 'Texas' }
-            // , { name: 'The Delaney At Georgetown Village', state: 'Texas' },
+            , { name: 'The Delaney At Georgetown Village', state: 'Texas' },
             // { name: 'The Isle At Watermere', state: 'Florida' }
             // , { name: 'Emerald Park of Hollywood', state: 'Florida' },
             // { name: 'Banyan Place', state: 'Florida' }

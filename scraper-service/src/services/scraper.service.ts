b844f91b6c5137a25d09c2dca1e2b775fb6632c6
Imran Chowdhury <imran.cuet.cse17@gmail.com>
2023-07-13 10:00:14 +0600

    reduced number of promises

--- scraper-service/src/services/scraper.service.ts
+++ scraper-service/src/services/scraper.service.ts
@@ -5,20 +5,23 @@ class ScraperService {
     async scrape() {
         const list: DataTobeScraped[] = [
             { name: 'Brookdale Creekside', state: 'Texas' }
-            , { name: 'The Delaney At Georgetown Village', state: 'Texas' },
-            { name: 'The Isle At Watermere', state: 'Florida' }
-            , { name: 'Emerald Park of Hollywood', state: 'Florida' },
-            { name: 'Banyan Place', state: 'Florida' }
+            // , { name: 'The Delaney At Georgetown Village', state: 'Texas' },
+            // { name: 'The Isle At Watermere', state: 'Florida' }
+            // , { name: 'Emerald Park of Hollywood', state: 'Florida' },
+            // { name: 'Banyan Place', state: 'Florida' }
         ]
         const promises: Promise<PropertyData[]>[] = [];
-        for (let property of list) {
-            if (property.state === 'Texas') {
-                promises.push(scraper.texasScraper(property.name));
+
+            for (let property of list) {
+                if (property.state === 'Texas') {
+                    promises.push(scraper.texasScraper(property.name));
+                }
+                else if (property.state === 'Florida') {
+                    promises.push(scraper.floridaScraper(property.name))
+                }
             }
-            else if (property.state === 'Florida') {
-                promises.push(scraper.floridaScraper(property.name))
-            }
-        }
+
+
 
         try {
             const promiseResponse = await Promise.all(promises);
